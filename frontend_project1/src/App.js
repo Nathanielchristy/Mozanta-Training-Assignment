22278efd9764b57f013ce40425be68cd1afc7129
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2022-08-04 11:48:12 +0530

    Adding calculator react project

---
+++ frontend_project1/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+// import Calbody from "./Components/calbody/Calbody";
+import Calculator from "./Components/calculator/calculator";
+
+
+function App() {
+  return (
+    <>
+
+      {/* <Calbody></Calbody> */}
+      <Calculator></Calculator>
+    </>
+  );
+}
+
+export default App;
